aa917e6747c699aee51f2f8ad3d1e2869e879e21
Caio Guimarães <caio.b.vilar@gmail.com>
2018-03-08 11:52:01 -0300

    changes and updates.

--- include/InputManager.hpp
+++ include/InputManager.hpp
@@ -2,11 +2,12 @@
 #define _INPUTMANAGER_HPP_
 
 #include <pthread.h>
+#include <queue>
 #include <map>
-#include <vector>
 #include <algorithm>
 #include <SDL2/SDL.h>
 #include "GameObject.hpp"
+#include <utility>
 
 class InputManager
 {
@@ -17,16 +18,24 @@ class InputManager
 		//mousebutton, mousewheel, joystick_axis, etc. Comming from
 		//Keyboard, controller, joystick or mouse
 
-		std::map<std::queue<SDL_Event*>,std::string> InputEventQueue;
-		std::map<std::queue<GameObjects>,std::string> InputObservers; //Game Objects that are listening for inputs.
+		std::map<std::queue<SDL_Event*>,std::string> InputEventQueues;
+		std::map<std::queue<GameObject*>,std::string> InputObservers; //Game Objects that are listening for inputs.
+
 	public:
 
 		InputManager();
 		~InputManager();
-		void registerNewQueue(SDL_Event*);	//Gets an Event and sorts it out to create a new queue.If non-existant.
+
+		//@function: registerNewQueue()
+		//@return: void
+		//@arguments: SDL_Event*
+		//@description: Gets an Event and sorts 
+		//it out to create a new queue.If non-existant.
+		void registerNewQueue(SDL_Event*);	
+		void registerNewQueue(std::string);	 //Basic queue, like keyboard and mouse
 		void attachObserver(GameObject*);
 		void dettachObserver(GameObject*);
 		void dispatchEvents();
-}
+};
 
 #endif //_INPUTMANAGER_HPP_
--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,9 +1,38 @@
-#include "InputManager.hpp"
+#include "../include/InputManager.hpp"
+
 InputManager::InputManager()
 {
+	//Start Keyboard Event Queue
+	this->registerNewQueue("Keyboard");
+	this->registerNewQueue("Mouse");
+
+	//Start Mouse Event Queue
+	
 }
 
 InputManager::~InputManager()
 {
 }
 
+void InputManager::registerNewQueue(SDL_Event*)
+{
+}
+
+void InputManager::registerNewQueue(std::string queue_id)
+{
+	std::queue<SDL_Event*> event_queue;
+	this->InputEventQueues.insert(
+			std::pair<std::queue<SDL_Event*>,std::string>());
+}
+
+void InputManager::attachObserver(GameObject*)
+{
+}
+
+void InputManager::dettachObserver(GameObject*)
+{
+}
+
+void InputManager::dispatchEvents()
+{
+}
