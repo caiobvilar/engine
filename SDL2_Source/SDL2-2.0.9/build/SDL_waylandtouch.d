d2cd2bb61a4a35c7c508c304dee34f121ef6d61b
caiovilar <caio.b.vilar@gmail.com>
2019-05-02 14:28:41 +0000

    update

---
+++ SDL2_Source/SDL2-2.0.9/build/SDL_waylandtouch.d
@@ -0,0 +1,27 @@
+build/SDL_waylandtouch.lo: \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/SDL_waylandtouch.c \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../SDL_internal.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../dynapi/SDL_dynapi.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+ include/close_code.h include/SDL_log.h include/SDL_stdinc.h \
+ include/SDL_config.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/SDL_waylandtouch.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/SDL_waylandvideo.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/SDL_waylanddyn.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/SDL_waylandsym.h \
+ gen/wayland-client-protocol.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/SDL_touch_c.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../SDL_internal.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_touch.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_stdinc.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_error.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/begin_code.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/close_code.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_video.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_pixels.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_endian.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_rect.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_rwops.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_surface.h \
+ /home/hellscoffe/Develop/engine/SDL2_Source/SDL2-2.0.9/src/video/wayland/../../events/../../include/SDL_blendmode.h
